d7d407d330534ada5730ceaf9e60c3de23e5919b
ishan0250 <ishanp0247@gmail.com>
2026-03-09 09:41:06 +0530

    Add files via upload

---
+++ looping.js
@@ -0,0 +1,17 @@
+let number =8;
+for(let i=1;i<=10;i++){
+    console.log(number*i);
+}
+let j=1;
+while(j<11){
+    console.log(number*j);
+    j++;
+}
+do {
+    console.log(number*j);
+    j++;
+}while(j<4)
+
+let arr=["apple", "doctor","away"];
+arr.push("keep");
+console.log(arr);
